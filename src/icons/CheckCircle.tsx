45b7910d5715d28f964594872fcdd8e747e3ef0f
William Martin <williamajmartin@gmail.com>
2023-07-26 19:26:24 +0100

    fix: CheckCircle

--- src/icons/CheckCircle.tsx
+++ src/icons/CheckCircle.tsx
@@ -12,17 +12,17 @@ export const CheckCircle = () => {
           id="Vector"
           d="M12.0938 7.3125L7.96641 11.25L5.90625 9.28125"
           stroke="#039855"
-          stroke-width="1.2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           id="Vector_2"
           d="M9 15.75C12.7279 15.75 15.75 12.7279 15.75 9C15.75 5.27208 12.7279 2.25 9 2.25C5.27208 2.25 2.25 5.27208 2.25 9C2.25 12.7279 5.27208 15.75 9 15.75Z"
           stroke="#039855"
-          stroke-width="1.2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
     </svg>
