38449cb143c665e621b07d717ce35339074c7926
Raul Glogovetan <raul@glogovetan.com>
2023-07-26 19:14:23 +0300

    Add: make `DropdownMenu` more reusable

--- src/components/DropdownMenu/index.jsx
+++ src/components/DropdownMenu/index.jsx
@@ -21,7 +21,7 @@ const useClickOutside = (handler) => {
   return domNode;
 };
 
-const DropdownMenu = ({ onClick, onStateChange }) => {
+const DropdownMenu = ({ onClick, onStateChange, items, children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClickInside = (event) => {
@@ -42,24 +42,16 @@ const DropdownMenu = ({ onClick, onStateChange }) => {
 
   return (
     <div ref={domNode} onClick={handleClickInside}>
-      <button type="button" className="relative z-10">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
-        </svg>
-      </button>
+      {children}
       {isOpen && (
         <div className="w-52 h-36 p-6 bg-white rounded-lg shadow border border-gray-200 flex-col justify-start items-start gap-4 inline-flex absolute right-0 z-20">
-          <div className="self-stretch justify-start items-start gap-4 inline-flex">
-            <div className="grow shrink basis-0 flex-col justify-start items-start gap-1 inline-flex">
-              <div className="self-stretch text-gray-900 text-sm font-normal leading-tight">View on Colony</div>
+          {items.map((item) => (
+            <div className="self-stretch justify-start items-start gap-4 inline-flex">
+              <div className="grow shrink basis-0 flex-col justify-start items-start gap-1 inline-flex">
+                <button className="self-stretch text-gray-900 text-sm font-normal leading-tight" onClick={item.handler}>{item.name}</button>
+              </div>
             </div>
-          </div>
-          <div className="self-stretch justify-start items-center gap-4 inline-flex">
-            <div className="grow shrink basis-0 flex-col justify-start items-start gap-1 inline-flex">
-              <div className="self-stretch text-gray-900 text-sm font-normal leading-tight">View project</div>
-            </div>
-          </div>
-          <div className="self-stretch text-gray-900 text-sm font-normal leading-tight">View website</div>
+          ))}
         </div>
       )}
     </div>
--- src/pages/VotePage/VotePage.tsx
+++ src/pages/VotePage/VotePage.tsx
@@ -29,6 +29,27 @@ const VotePage = () => {
     }
   };
 
+  const handleDropdownItemGeneric = (event: any) => {
+    event.preventDefault();
+    event.stopPropagation();
+    alert('clicked');
+  };
+
+  const dropdownItems = [
+    {
+      name: 'View on Colony',
+      handler: handleDropdownItemGeneric,
+    },
+    {
+      name: 'View Project',
+      handler: handleDropdownItemGeneric,
+    },
+    {
+      name: 'View Website',
+      handler: handleDropdownItemGeneric,
+    },
+  ];
+
   return (
     <div className="w-[1440px] mx-auto flex justify-center items-center">
       <div className="w-full h-full pt-12 pb-24 bg-white rounded-tl-[40px] rounded-bl-[40px] flex-col justify-start items-start gap-8 inline-flex">
@@ -82,7 +103,13 @@ const VotePage = () => {
                     </div>
                   </div>
                   <div className="absolute top-0 right-0 mt-2 mr-2">
-                    <DropdownMenu onClick={handleDropdownClick} onStateChange={handleDropdownState} />
+                    <DropdownMenu onClick={handleDropdownClick} onStateChange={handleDropdownState} items={dropdownItems}>
+                      <button type="button" className="relative z-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
+                        </svg>
+                      </button>
+                    </DropdownMenu>
                   </div>
                 </button>
                 <button type="submit" className="grow shrink basis-0 h-full p-6 bg-white rounded-lg border border-gray-200 justify-between items-center gap-6 flex hover:bg-blue-400 relative" onClick={handleVote}>
@@ -107,7 +134,13 @@ const VotePage = () => {
                     </div>
                   </div>
                   <div className="absolute top-0 right-0 mt-2 mr-2">
-                    <DropdownMenu onClick={handleDropdownClick} onStateChange={handleDropdownState} />
+                    <DropdownMenu onClick={handleDropdownClick} onStateChange={handleDropdownState} items={dropdownItems}>
+                      <button type="button" className="relative z-10">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
+                        </svg>
+                      </button>
+                    </DropdownMenu>
                   </div>
                 </button>
               </div>
