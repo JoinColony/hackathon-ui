a67493ab6456006e24da9a6643d82ee6d43cfbaf
William Martin <williamajmartin@gmail.com>
2023-08-04 14:02:35 +0200

    fix: getTierFromLeague typo

--- src/utils/leagues.ts
+++ src/utils/leagues.ts
@@ -8,6 +8,9 @@ Tier 3 contains Leagues 4-7
 
 etc.
 
+The logarithm base 2 will convert the league id into the tier it belongs to.
+We need to floor this to the tier as an integer, and then add one since tiers start at 1.
+
 */
 export const getTierFromLeague = (leagueId: number) =>
-  Math.ceil(Math.log2(leagueId)) + 1;
+  Math.floor(Math.log2(leagueId)) + 1;
