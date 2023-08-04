757b35db0047c11dc579aee41a5132003e3d6a20
Raul Glogovetan <raul@glogovetan.com>
2023-08-04 17:43:39 +0300

    Refactor: voting logic in `VotePage`

--- src/pages/VotePage/VotePage.tsx
+++ src/pages/VotePage/VotePage.tsx
@@ -6,8 +6,6 @@ import LoginModal from 'components/LoginModal';
 import useApi from 'hooks/useApi';
 
 import ColonyPoolCard from './ColonyPoolCard';
-import { getTierFromLeague } from 'utils/leagues';
-import Container from 'components/Container/Container';
 
 function generateCombinations(projects: any[]): any[][] {
   let combinations = [];
@@ -20,17 +18,34 @@ function generateCombinations(projects: any[]): any[][] {
     }
   }
 
+  // todo filter out already voted combinations, only if user is logged in
   return combinations;
 }
+
+// function generateCombinations(projects: any[]): Record<number, any[]> {
+//   let combinations: Record<number, any[]> = {};
+
+//   for (let i = 0; i < projects.length - 1; i++) {
+//     for (let j = i + 1; j < projects.length; j++) {
+//       if (projects[i].leagueId === projects[j].leagueId) {
+//         if (combinations[projects[i].leagueId]) {
+//           combinations[projects[i].leagueId].push([projects[i], projects[j]]);
+//         } else {
+//           combinations[projects[i].leagueId] = [[projects[i], projects[j]]];
+//         }
+//       }
+//     }
+//   }
 
 const VotePage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { loggedIn } = useContext(AuthContext);
   const api = useApi();
   const [projects, setProjects] = useState([]);
+  const [leagues, setLeagues] = useState<{ id: number }[]>([]);
+  const [currentLeague, setCurrentLeague] = useState(null);
+  const [currentCombinationIndex, setCurrentCombinationIndex] = useState<number>(0);
 
-  const [currentCombinationIndex, setCurrentCombinationIndex] =
-    useState<number>(0);
 
   useEffect(() => {
     const storedIndex = localStorage.getItem('currentCombinationIndex');
@@ -41,8 +56,6 @@ const VotePage = () => {
       const fetchedProjects = await api.getData('projects');
       setProjects(fetchedProjects);
     })();
-    // don't include api in dependencies as it causes an infinite loop
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const updateCombinationIndex = () => {
@@ -50,27 +63,26 @@ const VotePage = () => {
     localStorage.setItem('currentCombinationIndex', String(newIndex));
     setCurrentCombinationIndex(newIndex);
   };
+
   const uniqueCombinations = generateCombinations(projects);
+
+  console.log(uniqueCombinations);
 
   const handleVote = (event: SyntheticEvent, projectId: number) => {
     event.preventDefault();
 
     if (loggedIn) {
-      const votedCombinations = JSON.parse(
-        localStorage.getItem('votedCombinations') || '[]',
-      );
+      const votedCombinations = JSON.parse(localStorage.getItem('votedCombinations') || '[]');
       const alpha = uniqueCombinations[currentCombinationIndex][0].id;
       const beta = uniqueCombinations[currentCombinationIndex][1].id;
       const vote = projectId === alpha ? 1 : 0;
 
       votedCombinations.push({ alpha, beta, vote });
 
-      localStorage.setItem(
-        'votedCombinations',
-        JSON.stringify(votedCombinations),
-      );
+      localStorage.setItem('votedCombinations', JSON.stringify(votedCombinations));
       updateCombinationIndex();
     } else {
+      // force user to log in
       setIsModalOpen(true);
     }
   };
@@ -81,15 +93,12 @@ const VotePage = () => {
 
   const currentCombination = uniqueCombinations[currentCombinationIndex];
 
+  // post votes on the server
   useEffect(() => {
-    const votedCombinations = JSON.parse(
-      localStorage.getItem('votedCombinations') || '[]',
-    );
+    const votedCombinations = JSON.parse(localStorage.getItem('votedCombinations') || '[]');
 
-    if (
-      uniqueCombinations.length > 0 &&
-      currentCombinationIndex > uniqueCombinations.length - 1
-    ) {
+    // todo: post after voting on just a couple
+    if (uniqueCombinations.length > 0 && currentCombinationIndex > (uniqueCombinations.length - 1)) {
       votedCombinations.forEach((combination: any) => {
         api.postData('votes', combination);
       });
@@ -97,6 +106,29 @@ const VotePage = () => {
       localStorage.removeItem('currentCombinationIndex');
     }
   }, [api, currentCombinationIndex, uniqueCombinations]);
+
+  // update leagues and current league
+  useEffect(() => {
+    if (uniqueCombinations) {
+      // extract leagues
+      const allLeagues = uniqueCombinations.reduce((combination: { id: number }[] = [], project) => {
+        const [{ leagueId }] = project;
+        const leagueExists = leagues.find(({ id }: { id: number }) => id === leagueId);
+        if (!leagueExists) {
+          return [...leagues, { id: leagueId }];
+        }
+        return leagues;
+      }, []).sort(({ id: leagueIdA }: { id: number }, { id: leagueIdB }: { id: number }) => leagueIdA - leagueIdB);
+      setLeagues(allLeagues);
+      setCurrentLeague(allLeagues[0]);
+    }
+  }, []);
+
+  useEffect(() => {
+    // update current league we're voting in
+  }, []);
+
+  console.log(leagues)
 
   return (
     <div className="mx-auto flex justify-center items-center">
@@ -119,8 +151,7 @@ const VotePage = () => {
                   </div>
                   <div className="self-stretch justify-start items-center gap-1 inline-flex">
                     <div className="grow shrink basis-0 text-gray-500 text-base font-normal leading-normal">
-                      Use your reputation to support a project, the higher their
-                      ranking the more funding they will receive.
+                    Use your reputation to support a project, the higher their ranking the more funding they will receive.
                     </div>
                     <div className="justify-start items-center gap-1 flex">
                       <div className="w-4 h-4 relative" />
@@ -140,44 +171,36 @@ const VotePage = () => {
             </div>
           </div>
         </div>
-        {currentCombination ? (
+        {currentCombination && (
           <div className="self-stretch h-full flex-col justify-start items-center gap-6 flex">
             <div className="w-full h-full px-8 flex-col justify-start items-start gap-6 flex">
+              <p>currently voting in league: </p>
               <div className="self-stretch justify-start items-start gap-6 inline-flex">
                 {currentCombination.map((project: any) => {
-                  const {
-                    projectTitle,
-                    projectDescription,
-                    updates = [],
-                  } = JSON.parse(project.info);
-                  const [lastUpdate] = updates.sort(
-                    (
-                      { timestamp: timestampA }: { timestamp: number },
-                      { timestamp: timestampB }: { timestamp: number },
-                    ) => timestampB - timestampA,
-                  );
+                  const { projectTitle, projectDescription, updates = [] } = JSON.parse(project.info);
+                  const [lastUpdate] = updates.sort((
+                    { timestamp: timestampA }: { timestamp: number },
+                    { timestamp: timestampB }: { timestamp: number }
+                  ) => timestampB - timestampA);
                   return (
                     <ColonyPoolCard
                       key={project.id}
                       projectId={project.id}
-                      onClick={handleVote}
+                      handleClick={handleVote}
                       title={projectTitle}
                       subtitle={projectDescription}
                       lastUpdated={lastUpdate?.timestamp || 1000000000000}
                     />
-                  );
-                })}
+                )})}
               </div>
             </div>
             <div className="self-stretch justify-center items-center gap-2.5 inline-flex">
               <div className="justify-start items-center gap-1 flex">
                 <Link
-                  to={`/tier/${getTierFromLeague(
-                    currentCombination[0].leagueId,
-                  )}`}
+                  to="#"
                   className="text-center text-gray-900 text-xs font-medium leading-[18px] hover:text-light-blue-400"
                 >
-                  View league tier
+                  View league
                 </Link>
               </div>
               <div className="px-3 py-2 bg-white rounded-lg border border-gray-300 hover:border-light-blue-400 justify-center items-center gap-2 flex group cursor-pointer">
@@ -191,12 +214,6 @@ const VotePage = () => {
               </div>
             </div>
           </div>
-        ) : (
-          <Container>
-            <div className="flex justify-center">
-              <div className="font-bold">You're all caught up!</div>
-            </div>
-          </Container>
         )}
       </div>
       {isModalOpen && <LoginModal onClose={() => setIsModalOpen(false)} />}
